c10baf48c9768a37694009a2f72770427658f7bf
aunghp <aunghp.io@gmail.com>
2024-01-02 13:16:02 +0630

    fix query filter for category

--- app/Helpers/QueryBuilderHelper.php
+++ app/Helpers/QueryBuilderHelper.php
@@ -70,6 +70,11 @@ class QueryBuilderHelper
         $filter = isset($requestQuery['filter']) ? $requestQuery['filter'] : null;
         $value = isset($requestQuery['value']) ? $requestQuery['value'] : null;
 
+        /** For Main Categories Filtere */
+        if ($value === '0' && $filter === 'level') {
+            return $builder->where([$filter => $value]);
+        }
+
         if ($filter && $value) {
             $filterableFields = explode(',', $filter);
             $values = explode(',', $value);
