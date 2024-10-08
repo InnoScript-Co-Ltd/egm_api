e9a77ce9ce5be2e4afeeb52aea5390e09b9fe0fb
aunghp <aunghp.io@gmail.com>
2024-10-08 15:01:48 +0630

    fix

--- app/Http/Middleware/AgentMiddleware.php
+++ app/Http/Middleware/AgentMiddleware.php
@@ -31,6 +31,11 @@ class AgentMiddleware
         //     ]);
         // }
 
-        return $next($request);
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', '*')
+            ->header('Access-Control-Allow-Credentials', ' false');
+        ;
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,9 +52,4 @@ $app->singleton(
 |
 */
 
-header('Access-Control-Allow-Origin', '*');
-header('Access-Control-Allow-Methods', '*');
-header('Access-Control-Allow-Headers','*');
-header('Access-Control-Allow-Credentials',' false');
-
 return $app;
