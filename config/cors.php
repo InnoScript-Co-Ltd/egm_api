398a0a01ab46829a33400f28052a2abddebcb769
aunghp <aunghp.io@gmail.com>
2024-10-08 15:33:39 +0630

    fix

--- config/cors.php
+++ config/cors.php
@@ -15,13 +15,13 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'agent/*', 'dashboard/*', 'partner/*'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['evanglobalmanagement.com', '*.evanglobalmanagement.com'],
 
-    'allowed_origins_patterns' => ["*"],
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
