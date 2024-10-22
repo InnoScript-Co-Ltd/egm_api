dc2117b9300a54c654348e8c50d856a5d4aff128
aunghp <aunghp.io@gmail.com>
2024-10-22 18:16:27 +0630

    fix

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Enums\AgentTypeEnum;
 use App\Models\Agent;
+use App\Models\Deposit;
 use App\Models\Partner;
+use App\Models\Repayment;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
@@ -12,6 +15,17 @@ class DashboardController extends Controller
     {
         $partners = Partner::count();
         $agnets = Agent::all();
+
+        $startOfMonth = Carbon::now()->startOfMonth();
+        $endOfMonth = Carbon::now()->endOfMonth();
+
+        $agentDeposit = Deposit::whereNotNull('agent_id')->sum('deposit_amount');
+        $partnerDeposit = Deposit::whereNotNull('partner_id')->sum('deposit_amount');
+
+        $repaymentAmount = Repayment::whereBetween('date', [
+            $startOfMonth,
+            $endOfMonth,
+        ])->sum('amount');
 
         $main_agents = collect($agnets)->filter(function ($agent) {
             if ($agent->agent_type === AgentTypeEnum::MAIN_AGENT->value) {
@@ -29,6 +43,9 @@ class DashboardController extends Controller
             'partners' => $partners,
             'main_agents' => count($main_agents),
             'sub_agents' => count($sub_agents),
+            'agent_deposit' => $agentDeposit,
+            'partner_deposit' => $partnerDeposit,
+            'repayment_amount' => $repaymentAmount,
         ]);
     }
 }
